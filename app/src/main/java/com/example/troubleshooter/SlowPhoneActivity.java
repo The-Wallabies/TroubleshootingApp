bbbc263a0e4a075863e399438f7a6b408eec8657
Christian Torrisi <christian.torrisi@my.jcu.edu.au>
2021-10-26 22:37:49 +1000

    Add image sliders to Slow Phone Activity and layout.

--- app/src/main/java/com/example/troubleshooter/SlowPhoneActivity.java
+++ app/src/main/java/com/example/troubleshooter/SlowPhoneActivity.java
@@ -4,9 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SlowPhoneActivity extends AppCompatActivity {
+
+    Button prevButton, nextButton;
+    ImageView myImage;
+    TextView stepCounterText;
+    public int imageCount = 1;
+    public static final int NUMBER_OF_IMAGES = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,59 @@ public class SlowPhoneActivity extends AppCompatActivity {
         setContentView(R.layout.activity_slow_phone);
         Button goToSettingsButton = findViewById(R.id.settings);
         goToSettingsButton.setOnClickListener(v -> goToSettings());
+
+        prevButton = (Button) findViewById(R.id.nextButton);
+        nextButton = (Button) findViewById(R.id.prevButton);
+
+        myImage = (ImageView) findViewById(R.id.slowPhoneImages);
+        myImage.setImageResource(R.drawable.settings_button_image);
+
+        stepCounterText = findViewById(R.id.stepCounter);
+
+        prevButton.setOnClickListener(this::checkButton);
+        nextButton.setOnClickListener(this::checkButton);
+    }
+
+    public void checkButton(View v) {
+
+        switch (v.getId()){
+            case R.id.nextButton:
+                imageCount++;
+
+                if(imageCount == NUMBER_OF_IMAGES +1)
+                {
+                    imageCount = NUMBER_OF_IMAGES;
+                }
+                changeImage();
+                break;
+
+            case R.id.prevButton:
+                imageCount--;
+
+                if(imageCount == 0)
+                {
+                    imageCount = 1;
+                }
+                changeImage();
+                break;
+        }
+    }
+
+    public void changeImage() {
+        myImage = (ImageView) findViewById(R.id.slowPhoneImages);
+
+        switch (imageCount) {
+
+            case 1:
+                myImage.setImageResource(R.drawable.slow_phone_01);
+                stepCounterText.setText("Step 1.1");
+                break;
+
+            case 2:
+                myImage.setImageResource(R.drawable.slow_phone_02);
+                stepCounterText.setText("Step 1.2");
+                break;
+        }
     }
 
     private void goToSettings() {
